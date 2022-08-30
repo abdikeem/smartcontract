61f0cefdb7c4507b9504bbd6d0eb8c4d417965cd
KimzaTech <kimzatech@gmail.com>
2022-08-30 15:29:30 +0300

    BankApp

--- BankApp.sol
+++ BankApp.sol
@@ -2,15 +2,16 @@
 pragma solidity 0.8.4;
 
 contract BankApp {
-    contract BankApp {
-        address public manager;
+    address public manager;
 
-        struct Account {
-            uint256 id;
-            string memory name;
-            string memory KraPin;
-            uint256 balance;
+    struct Account {
+        uint256 id;
+        string memory name;
+        string memory KraPin;
+        uint256 balance;
     };
+    
+    mapping(address => Account) accounts;
 
     constructor() {
         manager = msg.sender;
@@ -22,5 +23,17 @@ contract BankApp {
             unit256 balance
             ) public{
                 require(msg.sender == manager);
+
+            Account memory account = accounts[user];
+            //check if the account is already registered
+            if (account.id != 0) {
+                revert("Account is already registered");
+            }
     }
+    account.id = id;
+    account.name = name;
+    account.KraPin = KraPin;
+    account.balance = balance;
+
+    accounts[user] = account;
 }
